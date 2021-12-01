38a2656e48a8cd86c5fa592abf35ad8cb8866458
gobtronic <1554705+gobtronic@users.noreply.github.com>
2021-12-01 13:57:04 +0100

    day1 bonus

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,23 +1,77 @@
-
 mod tools;
 
 fn main() {
-    if let Ok(values) = tools::read_values::<usize>("src/day1/values.txt") {
-        let mut count = 0;
-        let mut prev: Option<usize> = None;
-        for value in values.iter() {
-            match prev {
-                Some(prev) => {
+    println!("Main result: {}", day::process());
+    println!("Bonus result: {}", bonus::process());
+}
+
+mod day {
+    pub fn process() -> i32 {
+        if let Ok(values) = super::tools::read_values::<usize>("src/day1/values.txt") {
+            let mut count = 0;
+            let mut prev: Option<usize> = None;
+            for value in values.iter() {
+                if let Some(prev) = prev {
                     if value > &prev {
                         count += 1;
                     }
-                },
-                None => {}
+                }
+
+                prev = Some(*value);
             }
 
-            prev = Some(*value);
+            return count;
         }
 
-        println!("{}", count);
+        0
     }
-}   
+}
+
+mod bonus {
+    pub fn process() -> i32 {
+        if let Ok(values) = super::tools::read_values::<usize>("src/day1/values.txt") {
+            let mut count = 0;
+            let mut prev_sum: Option<usize> = None;
+            for (i, _) in values.iter().enumerate() {
+                let i_sum = three_sum(i, &values);
+                match i_sum {
+                    Some(i_sum) => {
+                        if let Some(prev_sum) = prev_sum {
+                            if i_sum > prev_sum {
+                                count += 1;
+                            }
+                        }
+
+                        prev_sum = Some(i_sum);
+                    }
+                    None => break,
+                }
+            }
+
+            return count;
+        }
+
+        0
+    }
+
+    fn three_sum(start: usize, values: &[usize]) -> Option<usize> {
+        if start + 2 >= values.len() {
+            return None;
+        }
+
+        Some(values[start] + values[start + 1_usize] + values[start + 2_usize])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn day() {
+        assert_eq!(super::day::process(), 1553);
+    }
+
+    #[test]
+    fn bonus() {
+        assert_eq!(super::bonus::process(), 1597);
+    }
+}
