7e4b3f38c98b22b8a4d641b140f6fd17b404f7b7
gobtronic <1554705+gobtronic@users.noreply.github.com>
2021-12-01 10:04:43 +0100

    first day main

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -3,6 +3,21 @@ mod tools;
 
 fn main() {
     if let Ok(values) = tools::read_values::<usize>("src/day1/values.txt") {
-        println!("{:?}", values);
+        let mut count = 0;
+        let mut prev: Option<usize> = None;
+        for value in values.iter() {
+            match prev {
+                Some(prev) => {
+                    if value > &prev {
+                        count += 1;
+                    }
+                },
+                None => {}
+            }
+
+            prev = Some(*value);
+        }
+
+        println!("{}", count);
     }
 }   
