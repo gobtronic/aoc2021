b4d188c53b700a2b03b60feac5014e8814b63e14
gobtronic <1554705+gobtronic@users.noreply.github.com>
2021-12-07 16:08:47 +0100

    bonus seems done and tests passes but result is wrong

--- src/day5/day5.rs
+++ src/day5/day5.rs
@@ -1,17 +1,19 @@
+use std::cmp::Ordering;
 use std::{collections::HashMap, fmt::Debug, hash::Hash, ops::RangeInclusive, str::FromStr};
 
 mod tools;
 
 fn main() {
-    println!("Day result: {}", day::process());
+    println!("Day result: {}", day::process(false));
+    println!("Bonus result: {}", day::process(true));
 }
 
 pub struct Diagram {
-    pub values: HashMap<Point, u8>,
+    pub values: HashMap<Point, u16>,
 }
 
 impl Diagram {
-    pub fn process(&mut self, lines: Vec<VentLine>) {
+    pub fn process(&mut self, lines: Vec<VentLine>, w_diagonals: bool) {
         for line in lines {
             match line.direction {
                 Direction::Horizontal(range) => {
@@ -21,7 +23,7 @@ impl Diagram {
                             y: line.alignment,
                         };
                         if let Some(occurences) = self.values.get_mut(&point) {
-                            *occurences += 1_u8;
+                            *occurences += 1_u16;
                         } else {
                             self.values.insert(point, 1);
                         }
@@ -34,9 +36,70 @@ impl Diagram {
                             y,
                         };
                         if let Some(occurences) = self.values.get_mut(&point) {
-                            *occurences += 1_u8;
+                            *occurences += 1_u16;
                         } else {
                             self.values.insert(point, 1);
+                        }
+                    }
+                }
+                Direction::Diagonal(start, end) => {
+                    if w_diagonals {
+                        let diag_steps = (start.x as i64 - end.x as i64).abs().try_into().unwrap();
+
+                        match (start.x.cmp(&end.x), start.y.cmp(&end.y)) {
+                            (Ordering::Less, Ordering::Less) => {
+                                for i in 0..=diag_steps {
+                                    let point = Point {
+                                        x: start.x + i,
+                                        y: start.y + i,
+                                    };
+                                    if let Some(occurences) = self.values.get_mut(&point) {
+                                        *occurences += 1_u16;
+                                    } else {
+                                        self.values.insert(point, 1);
+                                    }
+                                }
+                            }
+                            (Ordering::Less, Ordering::Greater) => {
+                                for i in 0..=diag_steps {
+                                    let point = Point {
+                                        x: start.x + i,
+                                        y: start.y - i,
+                                    };
+                                    if let Some(occurences) = self.values.get_mut(&point) {
+                                        *occurences += 1_u16;
+                                    } else {
+                                        self.values.insert(point, 1);
+                                    }
+                                }
+                            }
+                            (Ordering::Greater, Ordering::Less) => {
+                                for i in 0..=diag_steps {
+                                    let point = Point {
+                                        x: start.x - i,
+                                        y: start.y + i,
+                                    };
+                                    if let Some(occurences) = self.values.get_mut(&point) {
+                                        *occurences += 1_u16;
+                                    } else {
+                                        self.values.insert(point, 1);
+                                    }
+                                }
+                            }
+                            (Ordering::Greater, Ordering::Greater) => {
+                                for i in 0..=diag_steps {
+                                    let point = Point {
+                                        x: start.x - i,
+                                        y: start.y - i,
+                                    };
+                                    if let Some(occurences) = self.values.get_mut(&point) {
+                                        *occurences += 1_u16;
+                                    } else {
+                                        self.values.insert(point, 1);
+                                    }
+                                }
+                            }
+                            _ => {}
                         }
                     }
                 }
@@ -61,6 +124,7 @@ pub struct VentLine {
 pub enum Direction {
     Horizontal(RangeInclusive<u32>),
     Vertical(RangeInclusive<u32>),
+    Diagonal(Point, Point),
 }
 
 impl FromStr for VentLine {
@@ -91,6 +155,12 @@ impl FromStr for VentLine {
             } else {
                 direction = Direction::Vertical(second_p.x..=first_p.x);
             }
+        } else if (first_p.x as i64 - second_p.x as i64).abs()
+            == (first_p.y as i64 - second_p.y as i64).abs()
+        {
+            // Alignment is irrelevant for Direction::Diagonal
+            alignment = 0;
+            direction = Direction::Diagonal(first_p, second_p);
         } else {
             return Err(ParseError);
         }
@@ -165,12 +235,12 @@ mod day {
 
     use crate::{Diagram, VentLine};
 
-    pub fn process() -> usize {
+    pub fn process(w_diagonals: bool) -> usize {
         if let Ok(values) = super::tools::read_values::<String>("src/day5/values.txt") {
             let mut diagram = Diagram::default();
             let vent_lines: Vec<VentLine> =
                 values.iter().flat_map(|v| VentLine::from_str(v)).collect();
-            diagram.process(vent_lines);
+            diagram.process(vent_lines, w_diagonals);
 
             return diagram
                 .values
@@ -180,5 +250,47 @@ mod day {
         }
 
         0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use crate::{Point, Diagram, VentLine};
+
+    #[test]
+    fn mock_test() {
+        let values = vec![
+            "0,9 -> 5,9".to_string(),
+            "8,0 -> 0,8".to_string(),
+            "9,4 -> 3,4".to_string(),
+            "2,2 -> 2,1".to_string(),
+            "7,0 -> 7,4".to_string(),
+            "6,4 -> 2,0".to_string(),
+            "0,9 -> 2,9".to_string(),
+            "3,4 -> 1,4".to_string(),
+            "0,0 -> 8,8".to_string(),
+            "5,5 -> 8,2".to_string()
+        ];
+
+        let mut diagram = Diagram::default();
+        let vent_lines: Vec<VentLine> = values.iter().flat_map(|v| VentLine::from_str(v)).collect();
+        diagram.process(vent_lines, false);
+        let count =  diagram
+            .values
+            .iter()
+            .filter(|(_, collisions)| collisions > &&1)
+            .count();
+        assert_eq!(count, 5);
+
+        let mut diagram = Diagram::default();
+        let vent_lines: Vec<VentLine> = values.iter().flat_map(|v| VentLine::from_str(v)).collect();
+        diagram.process(vent_lines, true);
+        let count =  diagram
+            .values
+            .iter()
+            .filter(|(_, collisions)| collisions > &&1)
+            .count();
+        assert_eq!(count, 12);
     }
 }
